3ac81a31de8304e210e59b4b4a20524b2cb4afcd
Shannon Maynard <shannonmaynard@Shannons-MacBook-Pro-5.local>
2020-03-19 23:16:12 -0700

    tried to do the js for the 2 intro pages. Failed at it

--- comps/intro/intro.js
+++ comps/intro/intro.js
@@ -1,4 +1,17 @@
-function ShowQuiz(){
-    document.querySelector("#intropage").style.display = "none";
-    document.querySelector("#quizpage").style.display = "flex";
+//Henry's example
+
+//function ShowQuiz(){
+    //document.querySelector("#second-page").style.display = "none";
+   // document.querySelector("#quizpage").style.display = "flex";
+  //}
+
+  function ShowSecondPage(){
+    document.querySelector("#first-page").style.display = "none";
+    document.querySelector("#second-page").style.display = "flex";
+    document.querySelector("#quizpage").style.display = "none";
+    document.querySelector("#correctbox").style.display = "none";
+    document.querySelector("#body").style.display = "none";
+    document.querySelector("#congratspage").style.display = "none";
+    document.querySelector("#fail-page").style.display = "none";
   }
+
---
+++ comps/intro2/intro2.js
@@ -0,0 +1,10 @@
+function ShowQuizPage(){
+    document.querySelector("#first-page").style.display = "none";
+    document.querySelector("#second-page").style.display = "none";
+    document.querySelector("#quizpage").style.display = "flex";
+    document.querySelector("#correctbox").style.display = "none";
+    document.querySelector("#body").style.display = "none";
+    document.querySelector("#congratspage").style.display = "none";
+    document.querySelector("#fail-page").style.display = "none";
+  }
+
