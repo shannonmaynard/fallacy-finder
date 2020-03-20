4d2e66aeb07483a47be882ae0a08977b92e193f7
yoyo <aminaash123@gmail.com>
2020-03-19 20:21:49 -0700

    updated

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ var scenarios = [
   },
   {
     title:"Scenario 2",
-    desc:"Something somethinge true.",
-    img:"comps/Picture/imgs/owl.png"
+    desc:"A mother tells her children to go to bed on time or else they will get haunted by the boogie man.",
+    img:"comps/Picture/imgs/Boogie.jpg"
   },
   {
     title:"Scenario 3",
-    desc:"blah blah",
-    img:"comps/Picture/imgs/carcrash.jpg"
+    desc:"Timmy's father tells him not to hit others, but if he gets hit first, its okay.",
+    img:"comps/Picture/imgs/twowrongs.jpg"
+  },
+
+  {
+    title:"Scenario 4",
+    desc:"Jane ignores an email that says she should forward it or she will be unlucky. On the way home that day she had a flat tire. She wishes she had sent that email.",
+    img:"comps/Picture/imgs/question.jpg"
+  },
+
+  {
+    title:"Scenario 5",
+    desc:"Sally likes to read instead of playing sports. Lately, though, she’s been made fun of and her friends call her a nerd for reading. So, she decides to quit reading and play sports instead.",
+    img:"comps/Picture/imgs/Bandwagon.jpg"
   }
+
 ]
