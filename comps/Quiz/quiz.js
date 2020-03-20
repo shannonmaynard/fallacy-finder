07d5375ca073733a926de86d32490579374a791d
Shannon Maynard <shannonmaynard@Shannons-MacBook-Pro-5.local>
2020-03-19 22:11:39 -0700

    changed color for button options when clicked on js

--- comps/Quiz/quiz.js
+++ comps/Quiz/quiz.js
@@ -2,3 +2,21 @@ function NextQuiz(){
   s_ind++;
   document.querySelector("#quizpage #title").innerHTML = scenarios[s_ind].title;
 }
+
+function ChangeBtnColorOption1(){
+document.getElementById("option1").style.backgroundColor = "#41c9ff";
+document.getElementById("option2").style.backgroundColor = "#bfedff";
+document.getElementById("option3").style.backgroundColor = "#bfedff";
+}
+
+function ChangeBtnColorOption2(){
+  document.getElementById("option1").style.backgroundColor = "#bfedff";
+  document.getElementById("option2").style.backgroundColor = "#41c9ff";
+  document.getElementById("option3").style.backgroundColor = "#bfedff";
+  }
+
+  function ChangeBtnColorOption3(){
+    document.getElementById("option1").style.backgroundColor = "#bfedff";
+    document.getElementById("option2").style.backgroundColor = "#bfedff";
+    document.getElementById("option3").style.backgroundColor = "#41c9ff";
+    }
